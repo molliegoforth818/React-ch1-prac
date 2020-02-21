ea3f5e63d5d3201d068560c8df8425570f6f3482
Mollie Goforth <molliegoforth818@gmail.com>
2020-02-21 14:53:54 -0600

    locations employee and owner cards all made

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EmployeeCard = () => {
+    return (
+        <div className="empcard">
+      <div className="empcard-content">
+        <h3>
+          Name: <span className="card-empname">Mollz</span>
+        </h3>
+        <p>Title: Front Desk Receptionist</p>
+      </div>
+    </div>
+    )
+}
+export default EmployeeCard
---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LocationCard = () => {
+    return (
+        <div className="locCard">
+        <div className="locCard-content">
+          <h3>
+     <span className="card-locname">Student Kennels Nashville North</span>
+          </h3>
+          <h3><span className="card-locname2">Student Kennels Nashville South</span>
+          </h3>
+          
+        </div>
+      </div>
+    )
+}
+export default LocationCard
